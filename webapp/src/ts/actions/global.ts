00c19fc86f50300afeac8bf45894c37db0e0e3e0
m5r <mokht@rmi.al>
2021-10-19 14:56:49 +0200

    rename repeated field snackbar `content` to `message`

--- webapp/src/ts/actions/global.ts
+++ webapp/src/ts/actions/global.ts
@@ -64,8 +64,8 @@ export class GlobalActions {
     return this.store.dispatch(Actions.setSnapshotData(snapshotData));
   }
 
-  setSnackbarContent(content, action?) {
-    return this.store.dispatch(Actions.setSnackbarContent({ content, action }));
+  setSnackbarContent(message, action?) {
+    return this.store.dispatch(Actions.setSnackbarContent({ message, action }));
   }
 
   setLoadingContent(loading) {
